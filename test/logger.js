949a98285a070a63ee293a8f491a40efd48e61b3
respectus <muazs786@gmail.com>
2016-08-17 17:38:41 -0700

    Added a new branch and committed3

--- test/logger.js
+++ test/logger.js
@@ -7,7 +7,7 @@ var http = require('http');
 
 var logger = Logger.createLogger(testHelper.apikey, testHelper.options);
 var testLength = testHelper.testLength;
-var testStr = 'ESOTERIC 2';
+var testStr = 'ESOTERIC 3';
 var ordered = [];
 var sentLines = [];
 var sentLevels = [];
