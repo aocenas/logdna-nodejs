697c5de335057d1d21090ed2cbad4ed6c3a5f626
respectus <muazs786@gmail.com>
2016-08-18 11:21:50 -0700

    super long commit message, shouldn't be parsed or should be cut off at some point right? This should be more than 40 characters by this point I would hope

--- test/logger.js
+++ test/logger.js
@@ -7,7 +7,7 @@ var http = require('http');
 
 var logger = Logger.createLogger(testHelper.apikey, testHelper.options);
 var testLength = testHelper.testLength;
-var testStr = 'ESOTERIC 7';
+var testStr = 'ESOTERIC 99';
 var ordered = [];
 var sentLines = [];
 var sentLevels = [];
